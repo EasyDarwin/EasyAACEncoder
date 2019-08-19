e0c3dae0a44d93491cce0505c254ec8e038915c2
Babosa <babosa@easydarwin.org>
2019-08-19 11:15:51 +0800

    update libeasyaacencoder

---
+++ testEasyAACEncoder/nbproject/Makefile-variables.mk
@@ -0,0 +1,75 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=testeasyaacencoder
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/testeasyaacencoder
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=testeasyaacencoder.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/testeasyaacencoder.tar
+# x64 configuration
+CND_PLATFORM_x64=GNU-Linux
+CND_ARTIFACT_DIR_x64=dist/x64
+CND_ARTIFACT_NAME_x64=testeasyaacencoder
+CND_ARTIFACT_PATH_x64=dist/x64/testeasyaacencoder
+CND_PACKAGE_DIR_x64=dist/x64/GNU-Linux/package
+CND_PACKAGE_NAME_x64=testeasyaacencoder.tar
+CND_PACKAGE_PATH_x64=dist/x64/GNU-Linux/package/testeasyaacencoder.tar
+# i386 configuration
+CND_PLATFORM_i386=GNU-Linux
+CND_ARTIFACT_DIR_i386=dist/i386
+CND_ARTIFACT_NAME_i386=testeasyaacencoder
+CND_ARTIFACT_PATH_i386=dist/i386/testeasyaacencoder
+CND_PACKAGE_DIR_i386=dist/i386/GNU-Linux/package
+CND_PACKAGE_NAME_i386=testeasyaacencoder.tar
+CND_PACKAGE_PATH_i386=dist/i386/GNU-Linux/package/testeasyaacencoder.tar
+# hisiv100 configuration
+CND_PLATFORM_hisiv100=hisiv100-Linux
+CND_ARTIFACT_DIR_hisiv100=dist/hisiv100
+CND_ARTIFACT_NAME_hisiv100=testeasyaacencoder
+CND_ARTIFACT_PATH_hisiv100=dist/hisiv100/testeasyaacencoder
+CND_PACKAGE_DIR_hisiv100=dist/hisiv100/hisiv100-Linux/package
+CND_PACKAGE_NAME_hisiv100=testeasyaacencoder.tar
+CND_PACKAGE_PATH_hisiv100=dist/hisiv100/hisiv100-Linux/package/testeasyaacencoder.tar
+# hisiv200 configuration
+CND_PLATFORM_hisiv200=hisiv200-Linux
+CND_ARTIFACT_DIR_hisiv200=dist/hisiv200
+CND_ARTIFACT_NAME_hisiv200=testeasyaacencoder
+CND_ARTIFACT_PATH_hisiv200=dist/hisiv200/testeasyaacencoder
+CND_PACKAGE_DIR_hisiv200=dist/hisiv200/hisiv200-Linux/package
+CND_PACKAGE_NAME_hisiv200=testeasyaacencoder.tar
+CND_PACKAGE_PATH_hisiv200=dist/hisiv200/hisiv200-Linux/package/testeasyaacencoder.tar
+# hisiv300 configuration
+CND_PLATFORM_hisiv300=hisiv300-Linux
+CND_ARTIFACT_DIR_hisiv300=dist/hisiv300
+CND_ARTIFACT_NAME_hisiv300=testeasyaacencoder
+CND_ARTIFACT_PATH_hisiv300=dist/hisiv300/testeasyaacencoder
+CND_PACKAGE_DIR_hisiv300=dist/hisiv300/hisiv300-Linux/package
+CND_PACKAGE_NAME_hisiv300=testeasyaacencoder.tar
+CND_PACKAGE_PATH_hisiv300=dist/hisiv300/hisiv300-Linux/package/testeasyaacencoder.tar
+# hisiv400 configuration
+CND_PLATFORM_hisiv400=hisiv400-Linux
+CND_ARTIFACT_DIR_hisiv400=dist/hisiv400
+CND_ARTIFACT_NAME_hisiv400=testeasyaacencoder
+CND_ARTIFACT_PATH_hisiv400=dist/hisiv400/testeasyaacencoder
+CND_PACKAGE_DIR_hisiv400=dist/hisiv400/hisiv400-Linux/package
+CND_PACKAGE_NAME_hisiv400=testeasyaacencoder.tar
+CND_PACKAGE_PATH_hisiv400=dist/hisiv400/hisiv400-Linux/package/testeasyaacencoder.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
